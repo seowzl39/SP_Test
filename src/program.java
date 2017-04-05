c458bc194928f66b7e08e1b46a6da765da99b910
seowzl39 <seowzl39@gmail.com>
2017-04-05 21:02:37 +0800

    Setup

---
+++ src/program.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class program {
+
+	Map<Integer,ArrayList<String>> two = new HashMap<Integer,ArrayList<String>>();
+	Map<Integer,ArrayList<String>> third = new HashMap<Integer,ArrayList<String>>();
+	Map<Integer,ArrayList<String>> four = new HashMap<Integer,ArrayList<String>>();
+	Map<Integer,ArrayList<String>> five = new HashMap<Integer,ArrayList<String>>();
+	Map<Integer,ArrayList<String>> six = new HashMap<Integer,ArrayList<String>>();
+	Map<Integer,ArrayList<String>> seven = new HashMap<Integer,ArrayList<String>>();
+	Map<Integer,ArrayList<String>> eight = new HashMap<Integer,ArrayList<String>>();
+	Map<Integer,ArrayList<String>> nine = new HashMap<Integer,ArrayList<String>>();
+	
+	public static void main(String[] args) {
+
+	}
+
+}
